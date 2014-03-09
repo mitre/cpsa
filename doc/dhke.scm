d1c8d54aeada7635370dd7d210f17074c42bf490
John D. Ramsdell <ramsdell@mitre.org>
2014-03-09 11:22:18 -0400

    add dhke to doc

---
+++ doc/dhke.scm
@@ -0,0 +1,55 @@
+(herald "Diffie-Hellman Key Exchange" (algebra diffie-hellman))
+
+;; The Diffie-Hellman problem is given (exp (gen) x), and (exp (gen)
+;; y), compute the value of (exp (exp (gen) x) y) which is also (exp
+;; (exp (gen) y) x).
+
+(defprotocol dhke diffie-hellman
+  (defrole init (vars (a b name) (h base) (x expn))
+    (trace
+     (send (enc "i" (exp (gen) x) (privk a)))
+     (recv (cat (enc h (privk b)) (enc a b (exp h x))))
+     (send (enc "i" a b (exp h x))))
+    (non-orig x)
+    (uniq-orig (exp (gen) x)))
+  (defrole resp (vars (a b name) (h base) (y expn))
+    (trace
+     (recv (enc "i" h (privk a)))
+     (send (cat (enc (exp (gen) y) (privk b)) (enc a b (exp h y))))
+     (recv (enc "i" a b (exp h y))))
+    (non-orig y)
+    (uniq-orig (exp (gen) y))))
+
+(defskeleton dhke (vars (a b name))
+  (defstrand resp 3 (a a) (b b))
+  (non-orig (privk a) (privk b)))
+
+(defskeleton dhke (vars (a b name))
+  (defstrand init 2 (a a) (b b))
+  (non-orig (privk a) (privk b)))
+
+(defprotocol dh-mim diffie-hellman
+  (defrole init
+    (vars (h base) (x expn) (n text))
+    (trace
+     (send (exp (gen) x))
+     (recv h)
+     (send (enc n (exp h x))))
+    (non-orig x)
+    (uniq-orig n (exp (gen) x)))
+  (defrole resp
+    (vars (h base) (y expn) (n text))
+    (trace
+     (recv h)
+     (send (exp (gen) y))
+     (recv (enc n (exp h y))))
+    (non-orig y)
+    (uniq-orig (exp (gen) y)))
+  (comment "Diffie-Hellman without signatures"
+	   "has a man-in-the-middle attack"))
+
+(defskeleton dh-mim
+  (vars (n text))
+  (defstrand init 3 (n n))
+  (deflistener n)
+  (uniq-orig n))
--- tst/station_to_station.scm
+++ tst/station_to_station.scm
@@ -3,16 +3,16 @@
 (defprotocol station-to-station diffie-hellman
   (defrole init
     (vars (x expn) (h base) (a b name))
-    (trace 
-     (send (exp (gen) x)) 
+    (trace
+     (send (exp (gen) x))
      (recv (cat h (enc (enc h (exp (gen) x) (privk b)) (exp h x))))
      (send (enc (enc (exp (gen) x) h (privk a)) (exp h x))))
     (non-orig x)
     (uniq-orig (exp (gen) x)))
   (defrole resp
     (vars (y expn) (h base) (a b name))
-    (trace 
-     (recv h) 
+    (trace
+     (recv h)
      (send (cat (exp (gen) y) (enc (enc (exp (gen) y) h (privk b)) (exp h y))))
      (recv (enc (enc h (exp (gen) y) (privk a)) (exp h y))))
     (non-orig y)
@@ -27,4 +27,3 @@
   (vars (a b name))
   (defstrand resp 3 (a a) (b b))
   (non-orig (privk a) (privk b)))
-
