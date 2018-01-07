5d93b1957a7a3f036b2c7b06ea973e1ea025f46a
John D. Ramsdell <ramsdell@mitre.org>
2018-01-06 19:29:40 -0500

    reverse attempt to handle case when fail is no longer a method of Monad

--- src/CPSA/Loader.hs
+++ src/CPSA/Loader.hs
@@ -8,7 +8,6 @@
 
 module CPSA.Loader (loadSExprs) where
 
-import qualified Control.Monad.Fail as Fail
 import Control.Monad
 import Control.Applicative ()
 import qualified Data.List as L
@@ -143,9 +142,6 @@ instance Monad ReturnFail where
     return = Return
     Fail l >>= _ = Fail l
     Return r >>= k = k r
-    fail = Fail.fail
-
-instance Fail.MonadFail ReturnFail where
     fail s = Fail s
 
 loadRolePriority :: Monad m => Int -> SExpr Pos -> m (Int, Int)
