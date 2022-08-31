05b4ff66a9b52a8c9a28ca55b6ed8395dd3ae07c
Joshua D. Guttman <guttman@mitre.org>
2022-08-31 17:27:09 -0400

    extensive improvements to chan-yahalom for the manual

--- tst/chan-yahalom.scm
+++ tst/chan-yahalom.scm
@@ -17,26 +17,111 @@
     (trace (recv ch1 (cat a b n-a n-b))
 	   (send ch3 (cat a b k n-a n-b))
 	   (send ch2 (cat a b k)))
-    (conf ch3 ch2)
-    (auth ch2)
+    ;;    (conf ch3 ch2)
     (uniq-orig k)))
+
+;;; How much do we know assuming that the resp had a run with an
+;;; authenticated channel ch2?
 
 (defskeleton yahalom
   (vars (a b c name) (n-b text) (ch1 ch2 chan))
   (defstrand resp 4 (a a) (b b) (n-b n-b) (ch1 ch1) (ch2 ch2))
-  (conf ch1)
+  (auth ch2)   
   (uniq-orig n-b))
 
+;;; This is the answer to the previous question, together with the
+;;; additional assumptions that ch2 and ch3, used by the key server to
+;;; distribute the key, are confidential channels.  In this case, we
+;;; find *almost* all we would expect.
+
 (defskeleton yahalom
-  (vars (a b c name) (n-b text) (k skey) (ch1 ch2 chan))
-  (defstrand resp 4 (a a) (b b) (n-b n-b) (k k) (ch1 ch1) (ch2 ch2))
-  (deflistener k)
-  (conf ch1 ch2)
+  (vars (k skey) (n-b n-a n-a-0 n-b-0 text) (a b name)
+    (ch1 ch2 ch1-0 ch3 chan))
+  (defstrand resp 4 (k k) (n-a n-a) (n-b n-b) (b b) (a a) (ch1 ch1)
+    (ch2 ch2))
+  (defstrand serv 3 (k k) (n-a n-a-0) (n-b n-b-0) (a a) (b b)
+    (ch1 ch1-0) (ch2 ch2) (ch3 ch3))
+  (uniq-orig k n-b)
   (auth ch2)
-  (uniq-orig n-b))
+  (conf ch2 ch3))
+
+;;; This is teh answer to the previous question, together with the
+;;; assumption that the server's request channel is an authenticated
+;;; channel.  This yields all of the desired result, including ch1-0 = ch1.  
 
 (defskeleton yahalom
-  (vars (a b c name) (n-b text) (k skey) (ch chan))
-  (defstrand init 3 (ch ch))
+  (vars (k skey) (n-b n-a n-a-0 n-b-0 text) (a b name)
+    (ch1 ch2 ch1-0 ch3 chan))
+  (defstrand resp 4 (k k) (n-a n-a) (n-b n-b) (b b) (a a) (ch1 ch1)
+    (ch2 ch2))
+  (defstrand serv 3 (k k) (n-a n-a-0) (n-b n-b-0) (a a) (b b)
+    (ch1 ch1-0) (ch2 ch2) (ch3 ch3))
+  (uniq-orig k n-b)
+  (auth ch2 ch1-0)
+  (conf ch2 ch3))
+
+;;; In particular, we can now check that the session key cannot be
+;;; compromised, subject to the assumptions we have imposed.
+
+(defskeleton yahalom
+  (vars (k skey) (n-b n-a n-a-0 n-b-0 text) (a b name)
+    (ch1 ch2 ch1-0 ch3 chan))
+  (defstrand resp 4 (k k) (n-a n-a) (n-b n-b) (b b) (a a) (ch1 ch1)
+    (ch2 ch2))
+  (defstrand serv 3 (k k) (n-a n-a-0) (n-b n-b-0) (a a) (b b)
+    (ch1 ch1-0) (ch2 ch2) (ch3 ch3))
+  (deflistener k)
+  (uniq-orig k n-b)
+  (auth ch2 ch1-0)
+  (conf ch2 ch3))
+
+;;; Let's turn now to the initiator's point of view:  
+
+(defskeleton yahalom
+  (vars (a b c name) (n-a text) (k skey) (ch chan))
+  (defstrand init 3 (ch ch) (n-a n-a))
+  (uniq-orig n-a)
   (auth ch)
+  ;; This annotation is unnecessary
+  ;; (conf ch)
+  )
+
+;;; The initiator infers the presence of the server.  We can now also
+;;; assume that the server's channels have the same trust properties
+;;; as before:
+
+(defskeleton yahalom
+  (vars (k skey) (n-a n-b text) (a b name) (ch ch1 chan))
+  (defstrand init 3 (k k) (n-a n-a) (n-b n-b) (a a) (b b) (ch ch))
+  (defstrand serv 2 (k k) (n-a n-a) (n-b n-b) (a a) (b b) (ch1 ch1)
+    (ch3 ch))
+  (precedes ((0 0) (1 0)) ((1 1) (0 1)))
+  (uniq-orig k n-a)
+  (auth ch ch1)
   (conf ch))
+  
+;;; This now authenticates the presence of the responder for the first
+;;; two nodes, and its agreement on all of the values present there.
+
+;;; Finally, let us consider the server's point of view, with an
+;;; authenticated channel for the request, and confidential channels
+;;; over which to transmit the session key.
+
+(defskeleton yahalom
+  (vars (c a b name) (n-a n-b text) (k skey) (ch1 ch2 ch3 chan))
+  (defstrand serv 3 (ch1 ch1) (ch2 ch2) (ch3 ch3))
+  (auth ch1)
+  (conf ch2 ch3))
+
+;;; This query confirms that the server has authenticated the first
+;;; two nodes of the responder, but has not authenticated the
+;;; initiator at all.
+
+;;; However, the session key is safe:
+
+(defskeleton yahalom
+  (vars (c a b name) (n-a n-b text) (k skey) (ch1 ch2 ch3 chan))
+  (defstrand serv 3 (k k) (ch1 ch1) (ch2 ch2) (ch3 ch3))
+  (deflistener k)
+  (auth ch1)
+  (conf ch2 ch3))
